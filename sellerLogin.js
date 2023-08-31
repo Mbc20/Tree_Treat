4610c714aba0c005a95c3faae3dc79207d10aef7
Mbc20 <138395968+Mbc20@users.noreply.github.com>
2023-09-01 00:20:15 +0600

    Delete sellerLogin.js

--- sellerLogin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router=express.Router();
-
-router.get('/sellerLogin', async(req,res)=>{
-    res.render('sellerLogin');
-});
-
-module.exports = router;
