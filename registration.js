2b0f7ce3cab3b9c5ed50514bfb0fdf385f066694
Mbc20 <138395968+Mbc20@users.noreply.github.com>
2023-09-01 00:20:48 +0600

    Delete registration.js

--- registration.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router=express.Router();
-
-router.get('/registration', async(req,res)=>{
-    res.render('registration');
-});
-
-module.exports = router;
